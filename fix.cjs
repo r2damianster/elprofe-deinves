712ee21c45bd590acb39f80fdce0f90f9dbc5e5b
ArturoPC <arturo.rodriguez30@gmail.com>
2026-04-17 12:21:08 -0500

    chore: Remove temp files

--- fix.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('src/components/professor/studio/ActivityEditor.tsx', 'utf8');
-c = c.replace(/\\`/g, '`');
-c = c.replace(/\\\$/g, '$');
-fs.writeFileSync('src/components/professor/studio/ActivityEditor.tsx', c);
