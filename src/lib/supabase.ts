3bbb21916b52350176ae0beb5fcf8915ef2d6e87
Arturo <arturo.rodriguez30@gmail.com>
2026-09-06 12:32:29 -0500

    auto: 2026-09-06 12:32:28

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient, SupabaseAuthAdapter } from '@neondatabase/neon-js';
 import type { Database } from './database.types';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const neonUrl = import.meta.env.VITE_NEON_URL;
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient<Database>(neonUrl, {
+  auth: { adapter: SupabaseAuthAdapter() },
+});
