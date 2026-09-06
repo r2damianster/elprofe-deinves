9b4bd312e0b84582d6f7b89d682426d20f288b96
Arturo <arturo.rodriguez30@gmail.com>
2026-09-06 12:48:25 -0500

    auto: 2026-09-06 12:48:25

--- src/components/student/activities/Essay.tsx
+++ src/components/student/activities/Essay.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { supabase } from '../../../lib/supabase';
+import { callAiEnhance } from '../../../lib/aiEnhance';
 import { Wand2, Loader2, ChevronDown, ChevronUp } from 'lucide-react';
 import { useIntegrity } from './useIntegrity';
 import MetricsBar from './MetricsBar';
